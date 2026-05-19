d02f4cb1ece5b40e6a21c70ff97179d9288eda3a
musfik070-gif <musfik070@gmail.com>
2026-05-20 02:03:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("IdeaVault Server Running");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
